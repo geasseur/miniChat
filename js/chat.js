e43fec2b1c760d12bfce49154924188c39bb97d8
geasseur <baptiste.ventura@neuf.fr>
2017-07-21 09:41:37 +0200

    emoji in progress, comments and media queries added

--- js/chat.js
+++ js/chat.js
@@ -3,12 +3,15 @@ var emoji = 0;
 var message;
 var i;
 var tableauReponse = ["salut, comment ça va?","je vais bien merci","j'aime GOT","j'aime manger des sushis", "j'ai déjà un copain désolé", "j'adore les mangas", "je me casse", "mdr", ":)","lol"];
-var listEmoji = ["https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Day-template.svg/150px-Day-template.svg.png"];
+//var listEmoji = ["https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Day-template.svg/150px-Day-template.svg.png"];
 
+
+//fonction cachant le chat à la fin du chargement
 function depart(){
  $("#chat").fadeOut(0);
 }
 
+//fonction récupérant le nom de l'utisateur et passe de la page d'accueil au chat lui même
 function nomUtilisateur(){
     nom = $("#entreeNom").val();
     $("#entree").fadeOut(1000);
@@ -16,6 +19,7 @@ function nomUtilisateur(){
     $("#nomUtilisateur").text(nom);
 }
 
+//fonction principal lancant le tout
 $(document).ready(function(){
   depart();
   $("#validationNomUtilisateur").click(function(){
@@ -25,10 +29,12 @@ $(document).ready(function(){
     envoisMessageUtilisateur();
   });
   $(".imgEmoji").click(function(){
-    $("#fenetreUtilisateur").append(this);
+    var utilEmoji = (this);
+    $("#fenetreUtilisateur").append(utilEmoji);
   });
 });
 
+//fonction récupérant le message de l'utilisateur et l'ajoute à la fenetre des messages
 function envoisMessageUtilisateur(){
     message = $("#messageUtilisateur").val();
     $("#messageUtilisateur").val("");
@@ -52,17 +58,14 @@ function envoisMessageUtilisateur(){
 
 }
 
-function ajoutEmoji(){
-  $(".emoji").src = listEmoji[emoji];
-  preparationBot();
-}
-
+//donne un temps de réaction "humain" au bot
 function preparationBot(){
   setTimeout(function () {
     envoisMessageBot();
   }, 3000);
 }
 
+//le bot choisit une réponse et l'ajoute à sa fenetre de message
 function envoisMessageBot(){
   var random = Math.floor(Math.random()*tableauReponse.length);
   $("#fenetreBot").append("<p>"+tableauReponse[random]+"</p>");
